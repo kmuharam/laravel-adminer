ba4834f16692791b455b3749760cc4136c50ca52
Khalid Moharrum <khalid.moharram@gmail.com>
2020-11-17 01:50:32 +0200

    [REVERT] withoutMiddleware usage

--- src/Providers/LaravelAdminerServiceProvider.php
+++ src/Providers/LaravelAdminerServiceProvider.php
@@ -71,28 +71,28 @@ class LaravelAdminerServiceProvider extends ServiceProvider
     protected function bindManagerRoutes(): void
     {
         if (config('laravel-adminer.manager.enabled')) {
-            if (version_compare(App::version(), '7.7.0') >= 0) {
-                Route::middleware(config('laravel-adminer.manager.route.middleware'))
-                    ->namespace($this->packageNamespace)
-                    ->prefix(config('laravel-adminer.manager.route.path'))
-                    ->withoutMiddleware(['csrf'])
-                    ->group(function () {
-                        Route::any('/', [
-                            'as' => config('laravel-adminer.manager.route.name'),
-                            'uses' => 'AdminerController@manager',
-                        ]);
-                    });
-            } else {
-                Route::middleware(config('laravel-adminer.manager.route.middleware'))
-                    ->namespace($this->packageNamespace)
-                    ->prefix(config('laravel-adminer.manager.route.path'))
-                    ->group(function () {
-                        Route::any('/', [
-                            'as' => config('laravel-adminer.manager.route.name'),
-                            'uses' => 'AdminerController@manager',
-                        ]);
-                    });
-            }
+            // if (version_compare(App::version(), '7.7.0') >= 0) {
+            //     Route::middleware(config('laravel-adminer.manager.route.middleware'))
+            //         ->namespace($this->packageNamespace)
+            //         ->prefix(config('laravel-adminer.manager.route.path'))
+            //         ->withoutMiddleware(['csrf'])
+            //         ->group(function () {
+            //             Route::any('/', [
+            //                 'as' => config('laravel-adminer.manager.route.name'),
+            //                 'uses' => 'AdminerController@manager',
+            //             ]);
+            //         });
+            // } else {
+            Route::middleware(config('laravel-adminer.manager.route.middleware'))
+                ->namespace($this->packageNamespace)
+                ->prefix(config('laravel-adminer.manager.route.path'))
+                ->group(function () {
+                    Route::any('/', [
+                        'as' => config('laravel-adminer.manager.route.name'),
+                        'uses' => 'AdminerController@manager',
+                    ]);
+                });
+            // }
         }
     }
 
@@ -104,28 +104,28 @@ class LaravelAdminerServiceProvider extends ServiceProvider
     protected function bindEditorRoutes(): void
     {
         if (config('laravel-adminer.editor.enabled')) {
-            if (version_compare(App::version(), '7.7.0') >= 0) {
-                Route::middleware(config('laravel-adminer.editor.route.middleware'))
-                    ->namespace($this->packageNamespace)
-                    ->prefix(config('laravel-adminer.editor.route.path'))
-                    ->withoutMiddleware(['csrf'])
-                    ->group(function () {
-                        Route::any('/', [
-                            'as' => config('laravel-adminer.editor.route.name'),
-                            'uses' => 'AdminerController@editor',
-                        ]);
-                    });
-            } else {
-                Route::middleware(config('laravel-adminer.editor.route.middleware'))
-                    ->namespace($this->packageNamespace)
-                    ->prefix(config('laravel-adminer.editor.route.path'))
-                    ->group(function () {
-                        Route::any('/', [
-                            'as' => config('laravel-adminer.editor.route.name'),
-                            'uses' => 'AdminerController@editor',
-                        ]);
-                    });
-            }
+            // if (version_compare(App::version(), '7.7.0') >= 0) {
+            //     Route::middleware(config('laravel-adminer.editor.route.middleware'))
+            //         ->namespace($this->packageNamespace)
+            //         ->prefix(config('laravel-adminer.editor.route.path'))
+            //         ->withoutMiddleware(['csrf'])
+            //         ->group(function () {
+            //             Route::any('/', [
+            //                 'as' => config('laravel-adminer.editor.route.name'),
+            //                 'uses' => 'AdminerController@editor',
+            //             ]);
+            //         });
+            // } else {
+            Route::middleware(config('laravel-adminer.editor.route.middleware'))
+                ->namespace($this->packageNamespace)
+                ->prefix(config('laravel-adminer.editor.route.path'))
+                ->group(function () {
+                    Route::any('/', [
+                        'as' => config('laravel-adminer.editor.route.name'),
+                        'uses' => 'AdminerController@editor',
+                    ]);
+                });
+            // }
         }
     }
 }
